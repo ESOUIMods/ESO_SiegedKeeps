6c3756226746bfdd30fcc0001a4e9f7d3c628bf4
CaptainBlagbird <CaptainBlagbird@mailueberfall.de>
2015-11-02 23:30:53 +0100

    Updated icons

--- SiegedKeeps/SiegedKeeps.lua
+++ SiegedKeeps/SiegedKeeps.lua
@@ -14,15 +14,11 @@ SiegedKeeps.name = "Sieged Keeps"
 local LMP = LibStub("LibMapPins-1.0")
 
 -- Constatnts
-local NUM_KEEPS = 144  -- GetNumKeeps() returns 93 which isn't correct
+local NUM_KEEPS = 144  -- GetNumKeeps() returns 93 which isn't what we need
 local COLOR_AD = "|cF9F280"
-local COLOR_DC = "|c688AA5"
+local COLOR_DC = "|c7CA4C5"
 local COLOR_EP = "|cDC564A"
 local COLOR_ESO = "|cC5C29E"
-local REFRESH_INTERVAL_MS = 5000  -- TODO: Maybe move to settings menu
-local PIN_SIZE = 70  -- TODO: Maybe move to settings menu
-local PIN_SIZE_RESOURCE = 40  -- TODO: Maybe move to settings menu
-local PIN_LEVEL = 10  -- TODO: Maybe move to settings menu
 local PIN_TYPE_SIEGE = SiegedKeeps.name.." siege"
 local PIN_ICON_DIR = "SiegedKeeps/icons/"
 local PIN_ICON_AD       = PIN_ICON_DIR.."pin_AD.dds"
@@ -33,12 +29,18 @@ local PIN_ICON_AD_EP    = PIN_ICON_DIR.."pin_AD_EP.dds"
 local PIN_ICON_DC_EP    = PIN_ICON_DIR.."pin_DC_EP.dds"
 local PIN_ICON_AD_DC_EP = PIN_ICON_DIR.."pin_AD_DC_EP.dds"
 
+-- Settings (TODO: Maybe move to settings menu)
+local REFRESH_INTERVAL_MS = 5000
+local PIN_SIZE = 70
+local PIN_SIZE_RESOURCE = 40
+local PIN_LEVEL = 10
+
 -- Local variables
 local isReadyForRefresh = true
 
 
 -- Function to get the correct battleground context
-function GetBattlegroundContext()
+local function GetBattlegroundContext()
 	local bgquery = BGQUERY_UNKNOWN 
 	if GetCurrentCampaignId() == GetAssignedCampaignId() then
 		bgquery = BGQUERY_ASSIGNED_CAMPAIGN
@@ -58,43 +60,41 @@ local function MapCallbackQuestPins()
 		local ep = GetNumSieges(i, GetBattlegroundContext(), ALLIANCE_EBONHEART_PACT)
 		local dc = GetNumSieges(i, GetBattlegroundContext(), ALLIANCE_DAGGERFALL_COVENANT)
 		if ad+ep+dc > 0 then
-			-- if not GetKeepUnderAttack(i, GetBattlegroundContext()) then
-				-- Smaller icon for resources
-				if GetKeepType(i) == KEEPTYPE_RESOURCE then
-					LMP:SetLayoutKey(PIN_TYPE_SIEGE, "size", PIN_SIZE_RESOURCE)
-				else
-					LMP:SetLayoutKey(PIN_TYPE_SIEGE, "size", PIN_SIZE)
-				end
-				 
-				-- Set correct pin icon
-				if ad > 0 and dc > 0 and ep > 0 then
-					LMP:SetLayoutKey(PIN_TYPE_SIEGE, "texture", PIN_ICON_AD_DC_EP)
-				elseif ad > 0 and dc > 0 then
-					LMP:SetLayoutKey(PIN_TYPE_SIEGE, "texture", PIN_ICON_AD_DC)
-				elseif ad > 0 and ep > 0 then
-					LMP:SetLayoutKey(PIN_TYPE_SIEGE, "texture", PIN_ICON_AD_EP)
-				elseif dc > 0 and ep > 0 then
-					LMP:SetLayoutKey(PIN_TYPE_SIEGE, "texture", PIN_ICON_DC_EP)
-				elseif ad > 0 then
-					LMP:SetLayoutKey(PIN_TYPE_SIEGE, "texture", PIN_ICON_AD)
-				elseif dc > 0 then
-					LMP:SetLayoutKey(PIN_TYPE_SIEGE, "texture", PIN_ICON_DC)
-				elseif ep > 0 then
-					LMP:SetLayoutKey(PIN_TYPE_SIEGE, "texture", PIN_ICON_EP)
-				else -- Never reached
-				end
-				
-				-- Create tooltip text
-				local pinInfo = {COLOR_ESO.."Siege: "}
-				if ad > 0 then pinInfo[1] = pinInfo[1]..COLOR_AD..tostring(ad).."  " end
-				if dc > 0 then pinInfo[1] = pinInfo[1]..COLOR_DC..tostring(dc).."  " end
-				if ep > 0 then pinInfo[1] = pinInfo[1]..COLOR_EP..tostring(ep) end
-				pinInfo[1] = pinInfo[1]:gsub("%s+$", "")
-				
-				-- Get keep position and create pin
-				local pinType, normalizedX, normalizedY = GetKeepPinInfo(i, GetBattlegroundContext())
-				LMP:CreatePin(PIN_TYPE_SIEGE, pinInfo, normalizedX, normalizedY)
-			-- end
+			-- Smaller icon for resources
+			if GetKeepType(i) == KEEPTYPE_RESOURCE then
+				LMP:SetLayoutKey(PIN_TYPE_SIEGE, "size", PIN_SIZE_RESOURCE)
+			else
+				LMP:SetLayoutKey(PIN_TYPE_SIEGE, "size", PIN_SIZE)
+			end
+			 
+			-- Set correct pin icon
+			if ad > 0 and dc > 0 and ep > 0 then
+				LMP:SetLayoutKey(PIN_TYPE_SIEGE, "texture", PIN_ICON_AD_DC_EP)
+			elseif ad > 0 and dc > 0 then
+				LMP:SetLayoutKey(PIN_TYPE_SIEGE, "texture", PIN_ICON_AD_DC)
+			elseif ad > 0 and ep > 0 then
+				LMP:SetLayoutKey(PIN_TYPE_SIEGE, "texture", PIN_ICON_AD_EP)
+			elseif dc > 0 and ep > 0 then
+				LMP:SetLayoutKey(PIN_TYPE_SIEGE, "texture", PIN_ICON_DC_EP)
+			elseif ad > 0 then
+				LMP:SetLayoutKey(PIN_TYPE_SIEGE, "texture", PIN_ICON_AD)
+			elseif dc > 0 then
+				LMP:SetLayoutKey(PIN_TYPE_SIEGE, "texture", PIN_ICON_DC)
+			elseif ep > 0 then
+				LMP:SetLayoutKey(PIN_TYPE_SIEGE, "texture", PIN_ICON_EP)
+			else -- Never reached
+			end
+			
+			-- Create tooltip text
+			local pinInfo = {COLOR_ESO.."Siege: "}
+			if ad > 0 then pinInfo[1] = pinInfo[1]..COLOR_AD..tostring(ad).."  " end
+			if dc > 0 then pinInfo[1] = pinInfo[1]..COLOR_DC..tostring(dc).."  " end
+			if ep > 0 then pinInfo[1] = pinInfo[1]..COLOR_EP..tostring(ep) end
+			pinInfo[1] = pinInfo[1]:gsub("%s+$", "")
+			
+			-- Get keep position and create pin
+			local pinType, normalizedX, normalizedY = GetKeepPinInfo(i, GetBattlegroundContext())
+			LMP:CreatePin(PIN_TYPE_SIEGE, pinInfo, normalizedX, normalizedY)
 		end
 	end
 	
@@ -117,11 +117,10 @@ local function OnPlayerActivated(event)
 				SetTooltipText(InformationTooltip, lineData)
 			end
 		end,
-		tooltip = 1, -- Delete the line above and uncomment this line for Update 6
+		tooltip = 1,
 	}
 	local pinLayout = {size = PIN_SIZE, level = PIN_LEVEL}
 	LMP:AddPinType(PIN_TYPE_SIEGE, MapCallbackQuestPins, nil, pinLayout, pinTooltipCreator)
-	-- LMP:AddPinFilter(PIN_TYPE_SIEGE, GetString(SIEGEDKEEPS_FILTER), true, QuestMap.settings.pinFilters)
 	
 	EVENT_MANAGER:UnregisterForEvent(SiegedKeeps.name, EVENT_PLAYER_ACTIVATED)
 end
